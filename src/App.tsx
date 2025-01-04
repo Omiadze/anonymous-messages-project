cbf226ddfc9b14150ce280c902caf6101712ef02
Omiadze <teona.omiadze.1@iliauni.edu.ge>
2025-01-04 14:38:07 +0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./index.css"; // This should import your Tailwind CSS
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="bg-red-500">hello!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
